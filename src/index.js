92fa8961c66d1b9d55874da2664e21fad6530c9a
inehadev <156919946+inehadev@users.noreply.github.com>
2024-05-25 12:47:33 +0530

    modification

--- src/index.js
+++ src/index.js
@@ -1,21 +1,41 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import {BrowserRouter} from 'react-router-dom'
+// import { StateProvider } from './contextApi/StateProvider';
+// import  reducer , {  initialState } from './contextApi/reducer';
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <StateProvider initialState={initialState} reducer={reducer}>
+//     <BrowserRouter>
+//     <App />
+//     </BrowserRouter>
+//     </StateProvider>
+//   </React.StrictMode>
+// );
+
+
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
 import { StateProvider } from './contextApi/StateProvider';
-import  reducer , {  initialState } from './contextApi/reducer';
+import reducer, { initialState } from './contextApi/reducer';
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <StateProvider initialState={initialState} reducer={reducer}>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </StateProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 
